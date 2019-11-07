bcd2629016d590cd1d26aa1df2abc0ead0ac265f
Victor Lessa <victordsgnr@gmail.com>
2019-11-07 14:23:05 -0300

    packages and import vue perfect scroll

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import SuiVue from 'semantic-ui-vue'
 import store from './store'
 import 'semantic-ui-css/semantic.min.css'
+import PerfectScrollbar from 'vue2-perfect-scrollbar'
+import 'vue2-perfect-scrollbar/dist/vue2-perfect-scrollbar.css'
+
+Vue.use(PerfectScrollbar)
+
 Vue.use(SuiVue)
 Vue.config.productionTip = false
 

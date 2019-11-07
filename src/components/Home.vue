81a364ba0363097ce65914d40364ee6eab1185c0
Victor Lessa <victordsgnr@gmail.com>
2019-11-07 11:38:52 -0300

    rename component HelloWorld to Home

---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <sui-grid divided="vertically">
+    <sui-grid-row :columns="1">
+      <sui-grid-column>
+        <SearchComponent />
+      </sui-grid-column>
+    </sui-grid-row>
+    <ListGiphy />
+  </sui-grid>
+</template>
+
+<script>
+import ListGiphy from './ListGiphy'
+import SearchComponent from './Search'
+export default {
+  name: 'HelloWorld',
+  components: {
+    SearchComponent,
+    ListGiphy,
+  },
+  props: {
+    msg: String,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+h3
+  margin 40px 0 0
+
+ul
+  list-style-type none
+  padding 0
+
+li
+  display inline-block
+  margin 0 10px
+
+a
+  color #42b983
+</style>

3171f1e2447c75532c902cb18e8943dd52c816dd
Victor Lessa <victordsgnr@gmail.com>
2019-11-07 11:39:48 -0300

    component ListGiphy and Search to search and list answers

---
+++ src/components/ListGiphy.vue
@@ -0,0 +1,78 @@
+<template>
+  <sui-grid-row :columns="1">
+    <ul>
+      <li>
+        <sui-card>
+          <sui-image src="static/images/avatar/large/kristy.png" />
+          <sui-card-content>
+            <sui-card-header>Kristy</sui-card-header>
+            <sui-card-meta>Joined in 2013</sui-card-meta>
+            <sui-card-description
+              >Kristy is an art director living in New
+              York.</sui-card-description
+            >
+          </sui-card-content>
+          <sui-card-content extra>
+            <sui-icon name="user" />
+            22 Friends</sui-card-content
+          >
+        </sui-card>
+      </li>
+      <li>
+        <sui-card>
+          <sui-image src="static/images/avatar/large/kristy.png" />
+          <sui-card-content>
+            <sui-card-header>Kristy</sui-card-header>
+            <sui-card-meta>Joined in 2013</sui-card-meta>
+            <sui-card-description
+              >Kristy is an art director living in New
+              York.</sui-card-description
+            >
+          </sui-card-content>
+          <sui-card-content extra>
+            <sui-icon name="user" />
+            22 Friends</sui-card-content
+          >
+        </sui-card>
+      </li>
+      <li>
+        <sui-card>
+          <sui-image src="static/images/avatar/large/kristy.png" />
+          <sui-card-content>
+            <sui-card-header>Kristy</sui-card-header>
+            <sui-card-meta>Joined in 2013</sui-card-meta>
+            <sui-card-description
+              >Kristy is an art director living in New
+              York.</sui-card-description
+            >
+          </sui-card-content>
+          <sui-card-content extra>
+            <sui-icon name="user" />
+            22 Friends</sui-card-content
+          >
+        </sui-card>
+      </li>
+    </ul>
+  </sui-grid-row>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'ListGiphy',
+  computed: {
+    ...mapGetters('apiGiphy', ['FilterGiphy']),
+  },
+}
+</script>
+
+<style scoped lang="stylus">
+ul
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  justify-content center
+  li
+    margin 5px
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="ui left icon action input">
+    <i class="search icon"></i>
+    <input v-model="textInput" type="text" placeholder="Order #" />
+    <div @click="search" class="ui blue submit button">Search</div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+  name: 'Search',
+  data() {
+    return {
+      textInput: '',
+    }
+  },
+  methods: {
+    ...mapActions('apiGiphy', ['searchGiphhy']),
+    search() {
+      try {
+        this.searchGiphhy({ q: this.textInput, limit: 30, offset: 0 })
+      } catch (error) {
+        console.log(error)
+      }
+    },
+  },
+}
+</script>
+
+<style></style>

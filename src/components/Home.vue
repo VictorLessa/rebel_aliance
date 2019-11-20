39200d9e34619da39f80aa56226b232445672ea1
Victor Lessa <victordsgnr@gmail.com>
2019-11-20 14:18:49 -0300

    adding component tabs

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,18 +5,18 @@
         <SearchComponent />
       </sui-grid-column>
     </sui-grid-row>
-    <ListGiphy />
+    <Tabs />
   </sui-grid>
 </template>
 
 <script>
-import ListGiphy from './ListGiphy'
+import Tabs from './Tabs'
 import SearchComponent from './Search'
 export default {
-  name: 'HelloWorld',
+  name: 'Home',
   components: {
     SearchComponent,
-    ListGiphy,
+    Tabs,
   },
   props: {
     msg: String,
@@ -26,17 +26,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
-h3
-  margin 40px 0 0
+h3 {
+  margin: 40px 0 0;
+}
 
-ul
-  list-style-type none
-  padding 0
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-li
-  display inline-block
-  margin 0 10px
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 
-a
-  color #42b983
+a {
+  color: #42b983;
+}
 </style>
---
+++ src/components/Tabs.vue
@@ -0,0 +1,41 @@
+<template>
+  <sui-grid-row :columns="1" class="container">
+    <sui-tab style="width: 90%; margin: 0 auto" :menu="{ pointing: true }">
+      <sui-tab-pane class="tabGiphy" title="Giphy" :attached="false">
+        <ListGiphy />
+      </sui-tab-pane>
+      <sui-tab-pane :title="`Salvos ${lengthGiphySave}`" :attached="false">
+        <SavesComponent />
+      </sui-tab-pane>
+    </sui-tab>
+  </sui-grid-row>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import ListGiphy from './ListGiphy.vue'
+import SavesComponent from './Saves'
+export default {
+  name: 'Tabs',
+  data() {
+    return {}
+  },
+  components: {
+    ListGiphy,
+    SavesComponent,
+  },
+  computed: {
+    ...mapGetters('apiGiphy', ['lengthGiphySave']),
+  },
+}
+</script>
+
+<style lang="stylus">
+i = !important;
+
+#container {
+  height: 400px;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>

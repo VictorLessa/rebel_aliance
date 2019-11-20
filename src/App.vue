f0e156c5df49445d9244dceb9433058f781d968b
Victor Lessa <victordsgnr@gmail.com>
2019-11-20 14:18:09 -0300

    styles

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,69 @@ export default {
 </script>
 
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-body
-  overflow hidden
+i = !important;
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  overflow: hidden;
+}
+
+ul {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  justify-content: center;
+}
+
+li {
+  width: 200px;
+  margin: 5px;
+}
+
+.image-card {
+  background-position: center i;
+  background-size: cover i;
+  background-repeat: no-repeat i;
+  min-width: 100%;
+  height: 150px;
+}
+
+.v-label {
+  font-weight: bold;
+}
+
+.v-label > div {
+  margin: 0px 0 5px 0;
+}
+
+.description-modal {
+  display: flex;
+  flex-direction: column;
+}
+
+.image-modal {
+  > img {
+    max-height: 300px;
+  }
+}
+
+.tabGiphy {
+  width: 100%;
+}
+
+.box-content {
+  overflow: hidden;
+  border-size: content-box;
+  width: 100%;
+  text-overflow: ellipsis;
+}
 </style>

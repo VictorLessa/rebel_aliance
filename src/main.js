6de31a05fcb06c764cc3817fd098c53c90f60e29
Victor Lessa <victordsgnr@gmail.com>
2019-11-20 22:47:23 -0300

    directive with lazy load image

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import store from './store'
 import 'semantic-ui-css/semantic.min.css'
 import PerfectScrollbar from 'vue2-perfect-scrollbar'
 import 'vue2-perfect-scrollbar/dist/vue2-perfect-scrollbar.css'
+import LazyLoadDirective from './directives/lazy'
 
 Vue.use(PerfectScrollbar)
 
+Vue.directive('lazyload', LazyLoadDirective)
 Vue.use(SuiVue)
 Vue.config.productionTip = false
 
